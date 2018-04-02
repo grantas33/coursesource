066b6fca7f218434ef2aa977dc938f963f7c75f6
Grantas Gadliauskas <grantas0612@gmail.com>
2018-04-02 12:26:02 +0300

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: grantas
+ * Date: 18.3.30
+ * Time: 13.59
+ */
+
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return $this->render('index.html.twig', []);
+    }
+
+    /**
+     * @Route("/api/data", name="data-getter")
+     */
+    public function dataAction(){
+        return new JsonResponse(['title' => 'HElloo']);
+    }
+}
