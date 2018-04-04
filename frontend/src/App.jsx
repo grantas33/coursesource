66347e583f61d4f0783fac00b954e81c7bc39408
Matas Grisius <matas.grisius@gmail.com>
2018-04-04 23:58:27 +0300

    Clean up some code and added calendar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import MyCalendar from './MyCalendar';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
   render() {
     return (
       <div className="App">
