5c1ff000b0c3eb912282cd66aff48ab935825972
Matas Grisius <matas.grisius@gmail.com>
2018-04-22 10:29:42 +0300

    Bug fix

--- assets/js/Components/Course/Schedule/Calendar/Calendar.jsx
+++ assets/js/Components/Course/Schedule/Calendar/Calendar.jsx
@@ -3,7 +3,6 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import './Calendar.css';
-import events from './events';
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
