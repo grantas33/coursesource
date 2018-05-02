92b6febc17f7328fa07dc58a7e0e998d5882688d
Matas Grisius <matas.grisius@gmail.com>
2018-05-02 14:51:50 +0300

    Add course image

--- assets/js/Components/Main/Main/CourseItem.jsx
+++ assets/js/Components/Main/Main/CourseItem.jsx
@@ -6,9 +6,12 @@ const CourseItem = (props) => (
 <div className="box box-widget widget-user">
   <Link to={`/course/${props.course.id}`}>
     <div id="removed-height" className="widget-user-header bg-aqua-active ">
-      <h3 className="widget-user-username">{props.course.title}</h3>
+      <img className="course-image" src="https://www.designrepublic.com/11094-cart_default/krossing-100x100-cm.jpg" />
+      <div className="course-text">
+      <h3 className=" widget-user-username">{props.course.title}</h3>
       <p >{props.course.description}</p>
       <p >Creation date: {props.course.creation_date}</p>
+      </div>
     </div>
   </Link>
     <div className="row">
