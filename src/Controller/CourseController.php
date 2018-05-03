3d37f0c1c5c1f51ce16359d79057e3b2ea1a0e45
Grantas Gadliauskas <grantas0612@gmail.com>
2018-05-03 20:16:06 +0300

    add course_user submission and invitation methods

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -9,21 +9,18 @@
 namespace App\Controller;
 use App\Entity\Course;
 use App\Entity\CourseUser;
+use App\Entity\User;
 use App\Form\CourseType;
 use App\Interfaces\RoleInterface;
 use App\Interfaces\StatusInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class CourseController extends BaseController implements RoleInterface, StatusInterface
 {
-
-
     /**
      * @Route("api/courses", name="api_course_create", methods="POST")
      */
@@ -160,7 +157,7 @@ class CourseController extends BaseController implements RoleInterface, StatusIn
     /**
      * @Route("api/courses/{id}", name="api_course_delete", methods="DELETE")
      */
-    public function deleteLecture(int $id){
+    public function deleteCourse(int $id){
 
         $course = $this->getDoctrine()
             ->getRepository(Course::class)
@@ -188,6 +185,203 @@ class CourseController extends BaseController implements RoleInterface, StatusIn
         ]);
     }
 
+    /**
+     * @Route("api/courses/{id}/apply", name="api_course_apply", methods="POST")
+     */
+    public function applyToCourse(int $id){
 
+        $course = $this->getDoctrine()
+            ->getRepository(Course::class)
+            ->findOneBy(array('id'=>$id));
+
+        if(!$course || !$course->getIsPublic()){
+            return new JsonResponse([
+                'error_message' => 'This course is not open for applications'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $currentUserId = $this->getCurrentUserId();
+        $user = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$currentUserId,
+                'course'=>$id,
+                'course_status'=>StatusInterface::PENDING));
+
+        if($user){
+            return new JsonResponse([
+                'error_message' => 'Already submitted an application to this course'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $courseUser = new CourseUser();
+        $courseUser->setUser($currentUserId);
+        $courseUser->setCourse($id);
+        $courseUser->setRole(RoleInterface::APPLICANT);
+        $courseUser->setCourseStatus(StatusInterface::PENDING);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($courseUser);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            return new JsonResponse([
+                'error_message' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new JsonResponse([
+            'success_message' => 'Successfully sent an application to course '.$id
+        ], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("api/courses/{id}/invite", name="api_course_invite", methods="POST")
+     */
+    public function inviteToCourse(int $id, Request $request){
+
+        $courseAdmin = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$this->getCurrentUserId(),
+                'course'=>$id,
+                'role'=>RoleInterface::ADMIN));
+
+        if(!$courseAdmin){
+            return new JsonResponse([
+                'error_message' => 'You do not have the rights to invite this user'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(array('id'=>$data['user']));
+
+        if(!$user){
+            return new JsonResponse([
+                'error_message' => 'User does not exist'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $activeUser = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$data['user'],
+                'course_status'=>StatusInterface::ACTIVE));
+
+        if($activeUser){
+            return new JsonResponse([
+                'error_message' => 'User is already enrolled in the course'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $courseUser = new CourseUser();
+        $courseUser->setUser($data['user']);
+        $courseUser->setCourse($id);
+        $courseUser->setRole($data['role']);
+        $courseUser->setCourseStatus(StatusInterface::INVITED);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($courseUser);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            return new JsonResponse([
+                'error_message' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new JsonResponse([
+            'success_message' => 'Successfully invited user to course '.$id
+        ], Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * @Route("api/courses/{id}/acceptsubmission", name="api_course_acceptSubmission", methods="PUT")
+     */
+    public function acceptSubmission(int $id, Request $request){
+
+        $courseAdmin = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$this->getCurrentUserId(),
+                'course'=>$id,
+                'role'=>RoleInterface::ADMIN));
+
+        if(!$courseAdmin){
+            return new JsonResponse([
+                'error_message' => 'You do not have the rights to accept the submission'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $user = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$data['user'],
+                'course'=>$id,
+                'course_status'=>StatusInterface::PENDING));
+
+        if(!$user){
+            return new JsonResponse([
+                'error_message' => 'User does not exist or has not submitted an application'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->setRole(RoleInterface::STUDENT);
+        $user->setCourseStatus(StatusInterface::ACTIVE);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            return new JsonResponse([
+                'error_message' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new JsonResponse([
+            'success_message' => 'Successfully accepted the submission'
+        ]);
+    }
+
+    /**
+     * @Route("api/courses/{id}/acceptinvitation", name="api_course_acceptInvitation", methods="PUT")
+     */
+    public function acceptInvitation(int $id){
+
+        $user = $this->getDoctrine()
+            ->getRepository(CourseUser::class)
+            ->findOneBy(array(
+                'user'=>$this->getCurrentUserId(),
+                'course'=>$id,
+                'course_status'=>StatusInterface::INVITED));
+
+        if(!$user){
+            return new JsonResponse([
+                'error_message' => 'No invitation to accept'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->setCourseStatus(StatusInterface::ACTIVE);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            return new JsonResponse([
+                'error_message' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new JsonResponse([
+            'success_message' => 'Successfully accepted invitation to course '.$id
+        ]);
+    }
 
 }
---
+++ src/Repository/CourseUserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CourseUser;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method CourseUser|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CourseUser|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CourseUser[]    findAll()
+ * @method CourseUser[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CourseUserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, CourseUser::class);
+    }
+
+//    /**
+//     * @return Course[] Returns an array of Course objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Course
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
