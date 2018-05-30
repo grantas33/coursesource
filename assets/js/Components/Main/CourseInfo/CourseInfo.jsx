3ff663c6e64205de3cd9b45fca0cf55f288264ea
Matas Grisius <matas.grisius@gmail.com>
2018-05-30 10:14:09 +0300

    Redesign course info

--- assets/js/Components/Main/CourseInfo/CourseInfo.jsx
+++ assets/js/Components/Main/CourseInfo/CourseInfo.jsx
@@ -47,211 +47,226 @@ class CourseInfo extends React.Component {
           ]}
         />
         <div className="content">
-          <h1>{this.props.course.item.title}</h1>
-          {/*<img />*/}
-          <h5>{this.props.course.item.slogan}</h5>
-          <div className="nav-tabs-custom">
-            <ul className="nav nav-tabs">
-              <li
-                className={this.state.activeTab === 1 ? "active" : ""}
-                onClick={() => this.setState({ ...this.state, activeTab: 1 })}
-              >
-                <a>Description</a>
-              </li>
-              <li
-                className={this.state.activeTab === 2 ? "active" : ""}
-                onClick={() => this.setState({ ...this.state, activeTab: 2 })}
-              >
-                <a>Stats</a>
-              </li>
-              <li
-                className={this.state.activeTab === 3 ? "active" : ""}
-                onClick={() => this.setState({ ...this.state, activeTab: 3 })}
-              >
-                <a>Lectors</a>
-              </li>
-              <li
-                className={this.state.activeTab === 4 ? "active" : ""}
-                onClick={() => this.setState({ ...this.state, activeTab: 4 })}
-              >
-                <a>Other info</a>
-              </li>
-            </ul>
-            <div className="tab-content">
-              {this.state.activeTab === 1 && (
-                <h3>{this.props.course.item.description}</h3>
-              )}
+          <div className="row">
+            <div className="col-md-3">
+              <div className="box box-primary">
+                <div className="box-body box-profile">
+                  {this.props.course.item.avatar && (
+                    <img
+                      className="profile-user-img img-responsive img-circle"
+                      src={this.props.course.item.avatar}
+                      alt="Course avatar picture"
+                    />
+                  )}
+                  <h3 className="profile-username text-center">
+                    {this.props.course.item.title}
+                  </h3>
+                  <p className="text-muted text-center" />
+                  <ul className="list-group list-group-unbordered">
+                    <li className="list-group-item">
+                      <b>Created on</b> <a className="pull-right">{this.props.course.item.creation_date}</a>
+                    </li>
 
-              {this.state.activeTab === 2 && (
-                <div className="row">
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-green">
-                        <i className="ion-ios-information-outline" />
-                      </span>
-                      <div className="info-box-content">
-                        <span className="info-box-text">Assignments</span>
-                        <span className="info-box-number">
-                          {this.props.course.item.assignmentCount}
-                        </span>
-                      </div>
-                      {/* /.info-box-content */}
-                    </div>
-                    {/* /.info-box */}
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-green">
-                        <i className="ion ion-ios-book-outline" />
-                      </span>
-                      <div className="info-box-content">
-                        <span className="info-box-text">Lectures</span>
-                        <span className="info-box-number">
-                          {this.props.course.item.lectureCount}
-                        </span>
-                      </div>
-                      {/* /.info-box-content */}
-                    </div>
-                    {/* /.info-box */}
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-green">
-                        <i className="ion ion-ios-person-outline" />
-                      </span>
-                      <div className="info-box-content">
-                        <span className="info-box-text">Lectors</span>
-                        <span className="info-box-number">
-                          {this.props.course.item.teacherCount}
-                        </span>
-                      </div>
-                      {/* /.info-box-content */}
-                    </div>
-                    {/* /.info-box */}
-                  </div>
-                </div>
-              )}
-
-              {this.state.activeTab === 3 && (
-                <div className="row">
-                  {!this.props.course.loading &&
-                    this.props.course.item.teachers.map((teacher, i) => (
-                      <div className="col-sm-3" key={i}>
-                        <div className="box box-primary">
-                          <div className="box-body box-profile">
-                            <img
-                              className="profile-user-img img-responsive img-circle"
-                              src={user2img}
-                              alt="User profile picture"
-                            />
-                            <h3 className="profile-username text-center">
-                              Nina Mcintire
-                            </h3>
-                            <p className="text-muted text-center">
-                              Software Engineer
-                            </p>
-                            <ul className="list-group list-group-unbordered">
-                              <li className="list-group-item">
-                                <b>Followers</b>{" "}
-                                <a className="pull-right">1,322</a>
-                              </li>
-                            </ul>
-                          </div>
+                  </ul>
+                  {!this.props.course.item.is_submittable ? (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        if (!window.localStorage.getItem("userToken")) {
+                          this.props.history.push(`/register`);
+                        } else {
+                          swal({
+                            title: "Are you sure?",
+                            text: "You won't be able to revert this!",
+                            type: "warning",
+                            showCancelButton: true,
+                            confirmButtonColor: "#3085d6",
+                            cancelButtonColor: "#d33",
+                            confirmButtonText: "Yes, apply!"
+                          }).then(result => {
+                            if (result.value) {
+                              this.props.applyToCourse(
+                                this.props.course.item.id
+                              );
+                              swal(
+                                "Applied!",
+                                "You have applied to this course.",
+                                "success"
+                              );
+                            }
+                          });
+                        }
+                      }}
+                    >
+                      Join course
+                    </button>
+                  ) : (
+                    <div>
+                      <h3>
+                        In order to join this course you have to submit a
+                        solution for the following task:
+                      </h3>
+                      {!this.state.submitted ? (
+                        <div>
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => {
+                              if (!window.localStorage.getItem("userToken")) {
+                                this.props.history.push(`/register`);
+                              } else {
+                                swal({
+                                  title: "Are you sure?",
+                                  input: "text",
+                                  inputAttributes: {
+                                    autocapitalize: "off"
+                                  },
+                                  text: "You won't be able to revert this!",
+                                  type: "warning",
+                                  showCancelButton: true,
+                                  confirmButtonColor: "#3085d6",
+                                  cancelButtonColor: "#d33",
+                                  confirmButtonText: "Submit!"
+                                }).then(result => {
+                                  if (result.value) {
+                                    this.setState({
+                                      ...this.state,
+                                      submitted: true
+                                    });
+                                    this.props.applyToCourse(
+                                      this.props.course.item.id,
+                                      {
+                                        submission: this.state.submission
+                                      }
+                                    );
+                                    swal(
+                                      "Applied!",
+                                      "You have applied to this course.",
+                                      "success"
+                                    );
+                                  }
+                                });
+                              }
+                            }}
+                          >
+                            Submit a solution
+                          </button>
                         </div>
-                      </div>
-                    ))}
+                      ) : (
+                        <h3>You have already submitted a solution</h3>
+                      )}
+                    </div>
+                  )}
                 </div>
-              )}
+                {/* /.box-body */}
+              </div>
 
-              {this.state.activeTab === 4 && (
-                <h3>Created on: {this.props.course.item.creation_date}</h3>
-              )}
+              <div className="box box-primary">
+                <div className="box-header with-border">
+                  <h3 className="box-title">About</h3>
+                </div>
+                <div className="box-body">
+                  <strong>
+                    <i className="fa fa-map-marker margin-r-5" /> Location
+                  </strong>
+                  <p className="text-muted">Malibu, California</p>
+                </div>
+                {/* /.box-body */}
+              </div>
+              {/* /.box */}
+            </div>
+
+            <div className="col-md-9">
+              <div className="nav-tabs-custom">
+                <div className="tab-content">
+                  <div className="tab-pane active" id="activity">
+                    <div className="post">
+                      <div className="user-block">
+                        <span className="username">
+                          <a href="#">{this.props.course.item.slogan}</a>
+                          <a href="#" className="pull-right btn-box-tool" />
+                        </span>
+                        <span className="description">
+                          {this.props.course.item.title}
+                        </span>
+                      </div>
+                      <p>{this.props.course.item.description}</p>
+                    </div>
+                    <div className="post">
+                      <h3>Lectors</h3>
+                      <div className="row">
+                        {!this.props.course.loading &&
+                          this.props.course.item.teachers.map(teacher => (
+                            <div className="col-sm-3" key={teacher.id}>
+                              <div className="box box-primary">
+                                <div className="box-body box-profile">
+                                  <img
+                                    className="profile-user-img img-responsive img-circle"
+                                    src={user2img}
+                                    alt="User profile picture"
+                                  />
+                                  <h3 className="profile-username text-center">
+                                    {teacher.name + " " + teacher.surname}
+                                  </h3>
+                                  <p className="text-muted text-center">
+                                    {teacher.tag}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box">
+                <span className="info-box-icon bg-green">
+                  <i className="ion-ios-information-outline" />
+                </span>
+                <div className="info-box-content">
+                  <span className="info-box-text">Assignments</span>
+                  <span className="info-box-number">
+                    {this.props.course.item.assignmentCount}
+                  </span>
+                </div>
+                {/* /.info-box-content */}
+              </div>
+              {/* /.info-box */}
+            </div>
+            <div className="col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box">
+                <span className="info-box-icon bg-green">
+                  <i className="ion ion-ios-book-outline" />
+                </span>
+                <div className="info-box-content">
+                  <span className="info-box-text">Lectures</span>
+                  <span className="info-box-number">
+                    {this.props.course.item.lectureCount}
+                  </span>
+                </div>
+                {/* /.info-box-content */}
+              </div>
+              {/* /.info-box */}
+            </div>
+            <div className="col-md-3 col-sm-6 col-xs-12">
+              <div className="info-box">
+                <span className="info-box-icon bg-green">
+                  <i className="ion ion-ios-person-outline" />
+                </span>
+                <div className="info-box-content">
+                  <span className="info-box-text">Lectors</span>
+                  <span className="info-box-number">
+                    {this.props.course.item.teacherCount}
+                  </span>
+                </div>
+                {/* /.info-box-content */}
+              </div>
+              {/* /.info-box */}
             </div>
           </div>
-
-          {!this.props.course.item.is_submittable ? (
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                if (!window.localStorage.getItem("userToken")) {
-                  this.props.history.push(`/register`);
-                } else {
-                  swal({
-                    title: "Are you sure?",
-                    text: "You won't be able to revert this!",
-                    type: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#3085d6",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Yes, apply!"
-                  }).then(result => {
-                    if (result.value) {
-                      this.props.applyToCourse(this.props.course.item.id);
-                      swal(
-                        "Applied!",
-                        "You have applied to this course.",
-                        "success"
-                      );
-                    }
-                  });
-                }
-              }}
-            >
-              Join course
-            </button>
-          ) : (
-            <div>
-              <h3>
-                In order to join this course you have to submit a solution for
-                the following task:
-              </h3>
-              {!this.state.submitted ? (
-                <div>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => {
-                      if (!window.localStorage.getItem("userToken")) {
-                        this.props.history.push(`/register`);
-                      } else {
-                        swal({
-                          title: "Are you sure?",
-                          input: "text",
-                          inputAttributes: {
-                            autocapitalize: "off"
-                          },
-                          text: "You won't be able to revert this!",
-                          type: "warning",
-                          showCancelButton: true,
-                          confirmButtonColor: "#3085d6",
-                          cancelButtonColor: "#d33",
-                          confirmButtonText: "Submit!"
-                        }).then(result => {
-                          if (result.value) {
-                            this.setState({
-                              ...this.state,
-                              submitted: true
-                            });
-                            this.props.applyToCourse(this.props.course.item.id, {
-                              submission: this.state.submission
-                            });
-                            swal(
-                              "Applied!",
-                              "You have applied to this course.",
-                              "success"
-                            );
-                          }
-                        });
-                      }
-                    }}
-                  >
-                    Submit a solution
-                  </button>
-                </div>
-              ) : (
-                <h3>You have already submitted a solution</h3>
-              )}
-            </div>
-          )}
         </div>
       </div>
     );
